611b581b9a371a1f9e0eb384ae2f205af9575b1e
johnson <johnson.iyida@netopng.com>
2020-04-21 06:24:23 +0100

    update env to staging

--- middleware/auditlogger.js
+++ middleware/auditlogger.js
@@ -5,74 +5,74 @@ const AuditModel = require('../db/models/auditTrail');
 const User = require('../db/models/user');
 
 const logAudit = () => async (req, res, next) => {
-  const requestUrl = `${req.protocol}://${req.get('host')}${req.originalUrl}`;
-  let previousValue = {};
+  // const requestUrl = `${req.protocol}://${req.get('host')}${req.originalUrl}`;
+  // let previousValue = {};
 
-  if (req.method !== 'GET') {
-    if (req.method === 'PATCH') {
-      const response = await axios.get(requestUrl);
-      previousValue = await response.data;
-    }
+  // if (req.method !== 'GET') {
+  //   if (req.method === 'PATCH') {
+  //     const response = await axios.get(requestUrl);
+  //     previousValue = await response.data;
+  //   }
 
-    const user = await getCurrentUser(req, jwt, User);
-    const newAudit = await AuditModel.create({
-      user,
-      module: getModule(req.originalUrl),
-      actionType: req.method,
-      currentValue: req.body && JSON.stringify(req.body),
-      previousValue: previousValue && JSON.stringify(previousValue).toString(),
-      endPoint: req.originalUrl,
-      fullUrl: requestUrl,
-      ipAddress: req.headers['x-forwarded-for']
-                    || req.connection.remoteAddress
-                    || req.socket.remoteAddress
-                    || (req.connection.socket ? req.connection.socket.remoteAddress : null),
-      createdBy: req.user && req.user.id
-    });
+  //   const user = await getCurrentUser(req, jwt, User);
+  //   const newAudit = await AuditModel.create({
+  //     user,
+  //     module: getModule(req.originalUrl),
+  //     actionType: req.method,
+  //     currentValue: req.body && JSON.stringify(req.body),
+  //     previousValue: previousValue && JSON.stringify(previousValue).toString(),
+  //     endPoint: req.originalUrl,
+  //     fullUrl: requestUrl,
+  //     ipAddress: req.headers['x-forwarded-for']
+  //                   || req.connection.remoteAddress
+  //                   || req.socket.remoteAddress
+  //                   || (req.connection.socket ? req.connection.socket.remoteAddress : null),
+  //     createdBy: req.user && req.user.id
+  //   });
 
-    await newAudit.save();
-  }
+  //   await newAudit.save();
+  // }
 
-  function getModule(url) {
-    const filteredItems = ['v1', 'api', 'doc'];
-    let appModule = '';
-    if (!url) {
-      return appModule;
-    }
-    const urlItems = url.split('/');
+  // function getModule(url) {
+  //   const filteredItems = ['v1', 'api', 'doc'];
+  //   let appModule = '';
+  //   if (!url) {
+  //     return appModule;
+  //   }
+  //   const urlItems = url.split('/');
 
-    for (let i = 0; i < urlItems.length; i++) {
-      if (urlItems[i] && !filteredItems.includes(urlItems[i])) {
-        appModule = urlItems[i];
-        break;
-      }
-    }
+  //   for (let i = 0; i < urlItems.length; i++) {
+  //     if (urlItems[i] && !filteredItems.includes(urlItems[i])) {
+  //       appModule = urlItems[i];
+  //       break;
+  //     }
+  //   }
 
-    return appModule;
-  }
+  //   return appModule;
+  // }
 
-  async function getCurrentUser(request, jwtoken, userModel) {
-    // Getting token and check of it's there
-    let token;
-    let currentUser;
-    if (
-      request.headers.authorization
-                && request.headers.authorization.startsWith('Bearer')
-    ) {
-      token = request.headers.authorization.split(' ')[1];
-    }
+  // async function getCurrentUser(request, jwtoken, userModel) {
+  //   // Getting token and check of it's there
+  //   let token;
+  //   let currentUser;
+  //   if (
+  //     request.headers.authorization
+  //               && request.headers.authorization.startsWith('Bearer')
+  //   ) {
+  //     token = request.headers.authorization.split(' ')[1];
+  //   }
 
-    if (!token) {
-      return currentUser;
-    }
+  //   if (!token) {
+  //     return currentUser;
+  //   }
 
-    // verify token
-    const decoded = await promisify(jwtoken.verify)(token, process.env.JWT_SECRET);
+  //   // verify token
+  //   const decoded = await promisify(jwtoken.verify)(token, process.env.JWT_SECRET);
 
-    // Check if user still exists
-    currentUser = await userModel.findById(decoded.id);
-    return currentUser;
-  }
+  //   // Check if user still exists
+  //   currentUser = await userModel.findById(decoded.id);
+  //   return currentUser;
+  // }
   next();
 };
 
